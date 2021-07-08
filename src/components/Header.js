0c417c77cd91623d0351da0a786d16a7349e7df3
Luiz Vicentin <luizpavanello@gmail.com>
2021-07-07 22:16:19 -0300

    Create the Header of the App and add some classes to style later with UI

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="ui fixed menu">
+      <div className="ui container">
+        <h2>Contact Manager</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
