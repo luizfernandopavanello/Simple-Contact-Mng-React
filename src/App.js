fada4179ddb7884f1256f07a4262a57445533dba
Luiz Vicentin <luizpavanello@gmail.com>
2021-07-08 20:40:35 -0300

    Add some style with semantic UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React from './React';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header'
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
 
 function App() {
   return (
@@ -8,6 +11,8 @@ function App() {
       <section className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Header />
+        <AddContact />
+        <ContactList />
       </section>
     </main>
   );
