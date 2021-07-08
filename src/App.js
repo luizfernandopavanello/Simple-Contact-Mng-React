922c61bbbc7f88f54269ff36a4ccea8a01c81006
Luiz Vicentin <luizpavanello@gmail.com>
2021-07-08 20:58:49 -0300

    Update the App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React from './React';
-import logo from './logo.svg';
-import './App.css';
-import Header from './Header';
-import AddContact from './AddContact';
-import ContactList from './ContactList';
+import React, { useState, useEffect } from "react";
+import { uuid } from "uuidv4";
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <main className="App">
-      <section className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Header />
-        <AddContact />
-        <ContactList />
-      </section>
-    </main>
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+    </div>
   );
 }
 
