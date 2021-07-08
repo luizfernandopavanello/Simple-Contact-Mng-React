39af15377dca1e8f07742c91fdd542d913e495e3
Luiz Vicentin <luizpavanello@gmail.com>
2021-07-08 20:50:23 -0300

    Add the Button to Add a contact in the AddContact.js

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -14,6 +14,7 @@ class AddContact extends React.Component {
             <label>Email</label>
             <input type="text" name="email" placeholder="Email"/>
           </div>
+          <button className="ui button blue">Add</button>
         </form>
       </div>
     )
